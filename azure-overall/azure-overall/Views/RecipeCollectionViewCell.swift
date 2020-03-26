a9f8ac9429c4726452936bf2d0d23c445020786c
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-26 15:59:01 -0400

    creates RecipeCollectionViewCell

---
+++ azure-overall/azure-overall/Views/RecipeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionViewCell.swift
+//  azure-overall
+//
+//  Created by Sunni Tang on 3/26/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+    
+}
