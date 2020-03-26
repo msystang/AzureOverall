abc0bf66bf41c02cefac11f8a3a3ae2ae4c902bc
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-26 14:49:05 -0400

    creates SearchBar extension of BrowseVC

---
+++ azure-overall/azure-overall/Controllers/Browse/BrowseViewController+SearchBar.swift
@@ -0,0 +1,13 @@
+//
+//  BrowseViewController+SearchBar.swift
+//  azure-overall
+//
+//  Created by Sunni Tang on 3/26/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+extension BrowseViewController: UISearchBarDelegate {
+    
+}
