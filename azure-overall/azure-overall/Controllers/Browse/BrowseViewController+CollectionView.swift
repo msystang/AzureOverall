9f71995e665b3935ed8c8b66bbe063a3a9b9da75
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-26 16:56:22 -0400

    updates recipeCollectionView to populate 7 dequeued cells

--- azure-overall/azure-overall/Controllers/Browse/BrowseViewController+CollectionView.swift
+++ azure-overall/azure-overall/Controllers/Browse/BrowseViewController+CollectionView.swift
@@ -10,16 +10,29 @@ import UIKit
 
 extension BrowseViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 7
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifiers.recipeCollectionViewCell.rawValue, for: indexPath)
+        
+        return cell
     }
     
     
 }
 
 extension BrowseViewController: UICollectionViewDelegateFlowLayout {
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        // Adds padding to top, left, and right of the collectionView
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+            layout.minimumInteritemSpacing = 10
+            layout.minimumLineSpacing = 10
+            layout.invalidateLayout()
+
+            return CGSize(width: ((self.view.frame.width / 2) - 15), height:((self.view.frame.width / 2) - 15));
+        }
 }
