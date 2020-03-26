1b871c5437472ccb379e7fd5ddc051d4caee79ef
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-26 17:57:12 -0400

    creates constraints extension for RecipeTableViewCell

---
+++ azure-overall/azure-overall/Views/RecipeTableViewCell/RecipeTableViewCell+Constraints.swift
@@ -0,0 +1,9 @@
+//
+//  RecipeTableViewCell+Constraints.swift
+//  azure-overall
+//
+//  Created by Sunni Tang on 3/26/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import Foundation
