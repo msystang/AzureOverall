9a2deb528751b7d4d3d29468f161dace5ed48f02
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-26 16:02:50 -0400

    creates initializers for RecipeCVCell

--- azure-overall/azure-overall/Views/RecipeCollectionViewCell.swift
+++ azure-overall/azure-overall/Views/RecipeCollectionViewCell.swift
@@ -9,5 +9,12 @@
 import UIKit
 
 class RecipeCollectionViewCell: UICollectionViewCell {
+    //MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
