a8c0a47b84d2e550d85fdd94456c8f21571a49b6
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-26 15:10:33 -0400

    creates collectionView extension in BrowseVC

---
+++ azure-overall/azure-overall/Controllers/Browse/BrowseViewController+CollectionView.swift
@@ -0,0 +1,25 @@
+//
+//  BrowseViewController+CollectionView.swift
+//  azure-overall
+//
+//  Created by Sunni Tang on 3/26/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+extension BrowseViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
+
+extension BrowseViewController: UICollectionViewDelegateFlowLayout {
+    
+}
