1ee8e8f82814632f781024f2b9a5fedfc885cbaf
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-26 17:43:44 -0400

    creates ui objects and initialization funcs in CartTableViewCell

---
+++ azure-overall/azure-overall/Views/RecipeTableViewCell/RecipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CartTableViewCell.swift
+//  azure-overall
+//
+//  Created by Sunni Tang on 3/26/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
