ea999bf3482995210af5ac8cb461ebecb79afc2f
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-27 14:41:27 -0400

    makes Recipe conform to codable and creates decodeFromData func

--- azure-overall/azure-overall/Models/Recipe.swift
+++ azure-overall/azure-overall/Models/Recipe.swift
@@ -8,15 +8,22 @@
 
 import Foundation
 
-struct Recipe {
+struct Recipe: Codable {
+    // MARK: - Properties
+    private let image: String
     let id: Int
     let title: String
     let readyInMinutes: Int
     let servings: Int
-    private let image: String
         
     var imageUrl: String {
         return "https://spoonacular.com/recipeImages/\(image)"
+    }
+    
+    // MARK: - Internal Methods
+    static func decodeFromData(from jsonData: Data) throws -> [Recipe] {
+        let decodeResponse = try JSONDecoder().decode([Recipe].self, from: jsonData)
+        return decodeResponse
     }
     
 }
