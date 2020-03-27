200a2e55837c78e92e4b8aa23362e8e8db8d2856
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-27 14:42:05 -0400

    creates RecipeNetworkHelper class

--- azure-overall/azure-overall/Supporting Files/Networking/RecipeNetworkHelper.swift
+++ azure-overall/azure-overall/Supporting Files/Networking/RecipeNetworkHelper.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+class RecipeNetworkHelper {
+    // MARK: - Singleton
+    static let manager = RecipeNetworkHelper()
+    
+    // MARK: - Instance Methods
+    static func getUrlStr(from searchString: String, searchCount: Int) -> String {
+        return "https://api.spoonacular.com/recipes/search?query=\(searchString)&number=\(searchCount)&apiKey=\(Secrets.spoonacularKey)"
+    }
+    
+    func getRecipes(from urlStr: String, completion: @escaping (Result<[Recipe],NetworkingError>) -> ()) {
+        
+        guard let url = URL(string: urlStr) else {
+            completion(.failure(NetworkingError.badURL))
+            return
+        }
+        
+        NetworkManager.manager.getDataFromDataTask(from: url, with: .get) { (result) in
+            switch result {
+            case .failure(let error):
+                completion(.failure(error))
+            case .success(let data):
+                do {
+                    let recipes = try Recipe.decodeFromData(from: data)
+                    completion(.success(recipes))
+                } catch {
+                    completion(.failure(.couldNotParseJSON(rawError: error)))
+                }
+            }
+        }
+        
+    }
+    
+    // MARK: - Private Properties and Initializers
+    private init() {}
+}
