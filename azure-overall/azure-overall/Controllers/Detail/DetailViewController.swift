24217a508dd335121423403e4954ea6f6806bf35
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-27 15:27:39 -0400

    creates recipe variable in DetailVC and prints out title for testing

--- azure-overall/azure-overall/Controllers/Detail/DetailViewController.swift
+++ azure-overall/azure-overall/Controllers/Detail/DetailViewController.swift
@@ -10,21 +10,14 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    
+    var recipe: Recipe!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-        // Do any additional setup after loading the view.
+        
+        print(recipe.title)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
