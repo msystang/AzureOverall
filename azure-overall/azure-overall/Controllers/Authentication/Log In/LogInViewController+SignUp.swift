be25d24ff32ab9f86512243ce437f7ce943518f7
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-30 17:32:17 -0400

    moves signup related func to signup extension of LoginVC

--- azure-overall/azure-overall/Controllers/Authentication/Log In/LogInViewController+SignUp.swift
+++ azure-overall/azure-overall/Controllers/Authentication/Log In/LogInViewController+SignUp.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+extension LogInViewController {
+    func attemptSignUp(email: String, password: String) {
+        FirebaseAuthService.manager.signUpUser(email: email, password: password) { (result) in
+            self.handleSignUpResponse(with: result)
+        }
+    }
+    
+    private func handleSignUpResponse(with result: Result<User,Error>) {
+        switch result {
+        case .failure(let error):
+            showAlert(title: "Error", message: "Could not sign up: \(error.localizedDescription)")
+        case .success(let newUser):
+            let newAppUser = AppUser(from: newUser)
+            
+            FirestoreService.manager.createUser(user: newAppUser) { [weak self] (newResult) in
+                self?.addNewUserToFirestore(result: newResult)
+            }
+        }
+    }
+    
+    private func addNewUserToFirestore(result: Result<(), Error>) {
+        switch result {
+        case .success:
+            transitionToMainTabBarVC()
+        case .failure(let error):
+            self.showAlert(title: "Error creating user", message: "An error occured while creating new account \(error)")
+            enableButtons()
+        }
+    }
+
+}
