4d9cf2d7c70ff6e974c4f7ade9a0fb484a1dee42
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-30 17:34:11 -0400

    creates AppUser model

--- azure-overall/azure-overall/Models/AppUser.swift
+++ azure-overall/azure-overall/Models/AppUser.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+struct AppUser {
+    let email: String?
+    let uid: String
+    var cart: [String: Int]
+    let dateCreated: Date?
+    
+    //Instatiating to 'encode' to Firestore
+    init(from user: User) {
+        self.email = user.email
+        self.uid = user.uid
+        self.cart = [String:Int]()
+        self.dateCreated = user.metadata.creationDate
+    }
+    
+    //Instatiating when 'decoding' from Firestore
+    init?(from dict: [String: Any], id: String) {
+        guard let email = dict["email"] as? String,
+            let cart = dict["cart"] as? [String:Int],
+            let dateCreated = (dict["dateCreated"] as? Timestamp)?.dateValue() else { return nil }
+        
+        self.uid = id
+        self.email = email
+        self.cart = cart
+        self.dateCreated = dateCreated
+    }
+    
+    var fieldsDict: [String: Any] {
+        return [
+            "email": self.email ?? ""
+        ]
+    }
+}
