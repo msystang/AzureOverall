f08f0dafc8bd0c7f31c97396fc6f6aed493d7b40
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-30 19:25:25 -0400

    adds quantity property to Recipe model

--- azure-overall/azure-overall/Models/Recipe.swift
+++ azure-overall/azure-overall/Models/Recipe.swift
@@ -16,6 +16,7 @@ struct Recipe: Codable {
     let readyInMinutes: Int
     let servings: Int
         
+    var quantity: Int? = 0
     var imageUrl: String {
         return "https://spoonacular.com/recipeImages/\(image)"
     }
