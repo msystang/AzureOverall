18bda8a7585a9c89907d78011f52b155b39da757
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-30 14:02:25 -0400

    adds ui objects in loginVC

--- azure-overall/azure-overall/Controllers/Authentication/LogInViewController+Constraints.swift
+++ azure-overall/azure-overall/Controllers/Authentication/LogInViewController+Constraints.swift
@@ -6,4 +6,54 @@
 //  Copyright © 2020 Sunni Tang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension LogInViewController {
+    
+    // MARK: - Public Constraint Methods
+    func addSubviews() {
+        view.addSubview(logoImageView)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(textFieldStackView)
+        view.addSubview(signUpButton)
+        view.addSubview(logInButton)
+        view.addSubview(buttonStackView)
+    }
+    
+    func addConstraints() {
+        setLogoImageViewConstraints()
+        setTextFieldStackViewConstraints()
+        setButtonStackViewConstraints()
+    }
+    
+    // MARK: - Private Constraint Methods
+    private func setLogoImageViewConstraints() {
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+    
+    private func setTextFieldStackViewConstraints() {
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        textFieldStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+        
+        ])
+    }
+    
+    private func setButtonStackViewConstraints() {
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        logInButton.translatesAutoresizingMaskIntoConstraints = false
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+        
+        ])
+    }
+    
+}
--- azure-overall/azure-overall/Controllers/Authentication/LogInViewController.swift
+++ azure-overall/azure-overall/Controllers/Authentication/LogInViewController.swift
@@ -9,22 +9,75 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    // MARK: - UI Objects
+    
+    var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: AppImages.logo.rawValue)
+        return imageView
+    }()
+    
+    var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.borderStyle = .roundedRect
+        
+        return textField
+    }()
+    
+    var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    lazy var textFieldStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField])
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    lazy var logInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.addTarget(self, action: #selector(logInButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign Me Up!", for: .normal)
+        button.showsTouchWhenHighlighted = true
+        button.addTarget(self, action: #selector(signUpButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [signUpButton, logInButton])
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.axis = .horizontal
+        return stackView 
+    }()
+    
+    // MARK: - Private Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Objective-C Functions
+    @objc func logInButtonPressed() {
+        
     }
-    */
-
+    
+    @objc func signUpButtonPressed() {
+        
+    }
+    
 }
