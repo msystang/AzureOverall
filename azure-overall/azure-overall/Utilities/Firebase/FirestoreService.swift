7db47b02e5a5668513250daf9428b2a7bbd08336
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-30 17:34:45 -0400

    creates FirestoreService for creating new user

--- azure-overall/azure-overall/Utilities/Firebase/FirestoreService.swift
+++ azure-overall/azure-overall/Utilities/Firebase/FirestoreService.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+fileprivate enum FireStoreCollections: String {
+    case users
+}
+
+class FirestoreService {
+    //MARK: - Static Properties
+    static let manager = FirestoreService()
+    
+    private let db = Firestore.firestore()
+    
+    //MARK: - Instance Methods
+    //Users
+    func createUser(user: AppUser, completion: @escaping (Result<(), Error>) -> ()) {
+        var fields = user.fieldsDict
+        fields["dateCreated"] = Date()
+        fields["cart"] = [String:Int]()
+        db.collection(FireStoreCollections.users.rawValue).document(user.uid).setData(fields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+                print(error)
+            }
+            completion(.success(()))
+        }
+    }
+    
+    //MARK: - Private Properties and Initializers
+    private init () {}
+}
