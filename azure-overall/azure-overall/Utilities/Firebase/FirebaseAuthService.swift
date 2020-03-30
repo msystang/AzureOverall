94e6c0cf24fc792be8316eac202a93b5185c2fae
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-30 16:35:24 -0400

    creates FirebaseAuthService

--- azure-overall/azure-overall/Utilities/Firebase/FirebaseAuthService.swift
+++ azure-overall/azure-overall/Utilities/Firebase/FirebaseAuthService.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class FirebaseAuthService {
+    // MARK: - Internal Properties
+    static let manager = FirebaseAuthService()
+    
+    private let auth = Auth.auth()
+
+    var currentUser: User? {
+        return auth.currentUser
+    }
+    
+    // MARK: - Instance Methods
+    func createNewUser(email: String, password: String, completion: @escaping (Result<User,Error>) -> ()) {
+        auth.createUser(withEmail: email, password: password) { (result, error) in
+            if let createdUser = result?.user {
+                completion(.success(createdUser))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func loginUser(email: String, password: String, completion: @escaping (Result<(), Error>) -> ()) {
+        auth.signIn(withEmail: email, password: password) { (result, error) in
+            if (result?.user) != nil {
+                completion(.success(()))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func signOutUser(completion: @escaping (Result<(),Error>) -> ()) {
+        do {
+            try auth.signOut()
+            completion(.success(()))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    // MARK: - Private Properties and Initializers
+    private init () {}
+}
