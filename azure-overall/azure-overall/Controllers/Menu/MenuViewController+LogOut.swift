566b1b2d85dd0bbee706adc52d7021a7835228d9
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-31 17:19:05 -0400

    creates signout extension for menu vc

---
+++ azure-overall/azure-overall/Controllers/Menu/MenuViewController+LogOut.swift
@@ -0,0 +1,38 @@
+//
+//  MenuViewController+LogOut.swift
+//  azure-overall
+//
+//  Created by Sunni Tang on 3/31/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Log Out Methods
+extension MenuViewController {
+    
+    func attemptLogOut() {
+        FirebaseAuthService.manager.signOutUser { (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success():
+                self.clearCart()
+                self.transitionToLogInVC()
+            }
+        }
+    }
+    
+    func transitionToLogInVC() {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+            let sceneDelegate = windowScene.delegate as? SceneDelegate, let window = sceneDelegate.window
+            else {
+                return
+        }
+        
+        UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromRight, animations: {
+            window.rootViewController = LogInViewController()
+        }, completion: nil)
+    }
+    
+}
