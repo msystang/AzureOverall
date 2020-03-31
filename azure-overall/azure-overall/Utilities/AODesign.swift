6c2a3290b8b5e4b04b6c046ac2b8a599f8732d38
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-03-31 12:26:55 -0400

    creates custom AOViewController class and AODesign struct

---
+++ azure-overall/azure-overall/Utilities/AODesign.swift
@@ -0,0 +1,28 @@
+//
+//  AODesign.swift
+//  azure-overall
+//
+//  Created by Sunni Tang on 3/31/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+struct AODesign {
+//    static let backgroundColor =
+    static let aoAzure = #colorLiteral(red: 0, green: 0.4980392157, blue: 1, alpha: 1)
+    
+    enum fontSize: CGFloat {
+        case small = 10
+        case med = 15
+        case large = 30
+    }
+    
+    enum fontName: String {
+        case light = "KohinoorBangla-Light"
+        case regular = "KohinoorBangla-Regular"
+        case bold = "KohinoorBangla-Semibold"
+    }
+    
+
+}
---
+++ azure-overall/azure-overall/Views/AOViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AOViewController.swift
+//  azure-overall
+//
+//  Created by Sunni Tang on 3/31/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class AOViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = AODesign.aoAzure
+    }
+    
+
+}
